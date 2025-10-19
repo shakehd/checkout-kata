45b76c653311562c5a19a957aafe8758ef3a3634
shakeel ahmad <shakhd@duck.com>
2025-10-19 22:57:24 +0200

    Impl: unit pricing strategy creation fails when unit price is negative

--- checkout/Pricing/PricingStrategy.cs
+++ checkout/Pricing/PricingStrategy.cs
@@ -2,5 +2,9 @@ namespace checkout.Pricing;
 
 public class UnitPricing(int unitPrice) : IPricingStrategy
 {
-    public int CalculateTotalPrice(int itemNum) => unitPrice * itemNum;
+    public int UnitPrice { get; } = unitPrice < 0
+        ? throw new ArgumentException("Price cannot be negative.", nameof(unitPrice))
+        : unitPrice;
+
+    public int CalculateTotalPrice(int itemNum) => UnitPrice * itemNum;
 }
