f68cad803876ec3accfc1cded4dcb4c9e80c89c8
shakeel ahmad <shakhd@duck.com>
2025-10-19 23:01:23 +0200

    Test: unit pricing strategy should fail when negative item count is given

--- checkout.tests/UnitPricingStrategyTests.cs
+++ checkout.tests/UnitPricingStrategyTests.cs
@@ -7,15 +7,15 @@ public class UnitPricingStrategyTests
 {
     [Test, Sequential]
     public void Given_A_List_Of_SKus_Should_Return_Sum_Of_Unit_Prices(
-        [Range(0, 100, 10)] int numSkus,
+        [Range(0, 100, 10)] int itemCount,
         [Range(0, 500, 50)] int unitPrice)
     {
         IPricingStrategy sut = new UnitPricing(unitPrice);
         
         int actualTotalPrice =
-            sut.CalculateTotalPrice(numSkus);  
+            sut.CalculateTotalPrice(itemCount);  
         
-        Assert.That(actualTotalPrice, Is.EqualTo(numSkus * unitPrice));
+        Assert.That(actualTotalPrice, Is.EqualTo(itemCount * unitPrice));
     }
 
     [Test]
@@ -24,6 +24,15 @@ public class UnitPricingStrategyTests
     {
         Assert.That(() => new UnitPricing(unitPrice),
                     Throws.ArgumentException.With.Message.EqualTo("Price cannot be negative. (Parameter 'unitPrice')"));
+    }
+    
+    [Test]
+    public void Given_A_Negative_Item_Count_Unit_Pricing_Calculation_Should_Fail(
+        [Range(-100, -1, 10)] int itemCount)
+    {
+        IPricingStrategy sut = new UnitPricing(0);
+        Assert.That(() => sut.CalculateTotalPrice(itemCount),
+            Throws.ArgumentException.With.Message.EqualTo("Item count cannot be negative. (Parameter 'itemNum')"));
     }
     
 }
