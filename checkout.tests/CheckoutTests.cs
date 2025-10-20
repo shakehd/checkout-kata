93e39a5ad75b2d79125194cd9ddd8b9b65f3e64c
shakeel ahmad <shakhd@duck.com>
2025-10-20 22:12:53 +0200

    Test: a checkout should be able to scan multiple sku codes without raising error

--- checkout.tests/CheckoutTests.cs
+++ checkout.tests/CheckoutTests.cs
@@ -8,7 +8,7 @@ public class CheckoutTests
     [OneTimeSetUp]
     public void OneTimeSetup()
     {
-        _sut = new Checkout();
+        _sut = new Checkout(); 
     }
 
     [Test]
@@ -18,4 +18,18 @@ public class CheckoutTests
         
         Assert.That(actualTotalPrice, Is.Zero);
     }
+    
+    [Test]
+    public void Given_A_Checkout_Scanning_Multiple_SKUs_Should_Not_Raise_Errors(
+        [ValueSource(nameof(SKUCodes))] List<string> skuCodes)
+    {
+        Assert.That(() => skuCodes.ForEach(sku => _sut.Scan(sku)), Throws.Nothing);
+    }
+    
+    public static IEnumerable<List<string>> SKUCodes => new List<List<string>>
+    {
+        new() { "A", "B", "C", "D" },
+        new() { "A", "A", "B", "D" },
+        new() { "A", "A", "B", "B" }
+    };
 }
