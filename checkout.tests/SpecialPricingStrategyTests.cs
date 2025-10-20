24f528fd06ca1a4f78ae3944d60a90edc6ad661f
shakeel ahmad <shakhd@duck.com>
2025-10-20 18:19:51 +0200

    Test: special pricing strategy should apply special price for each bundle of sku

--- checkout.tests/SpecialPricingStrategyTests.cs
+++ checkout.tests/SpecialPricingStrategyTests.cs
@@ -17,4 +17,19 @@ public class SpecialPricingStrategyTests
         
         Assert.That(actualTotalPrice, Is.EqualTo(specialPrice));
     }
+    
+    [Test]
+    public void Given_A_List_Of_SKUs_Should_Apply_Special_Pricing_For_Each_Bundle_Of_SKUs(
+        [Range(1, 10, 2)] int bundleSize,
+        [Range(20, 100, 20)] int specialPrice,
+        [Random(0, 10, 5)] int multiFactor)
+    {
+        IPricingStrategy sut = new SpecialPricing(bundleSize, specialPrice, 0);
+        
+        int actualTotalPrice =
+            sut.CalculateTotalPrice(bundleSize * multiFactor);
+        
+        Assert.That(actualTotalPrice,
+            Is.AtLeast(multiFactor * specialPrice));
+    }
 }
