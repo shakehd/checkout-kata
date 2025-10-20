22e2c4092891af3256c22c5b9b66a3fba6eca9c9
shakeel ahmad <shakhd@duck.com>
2025-10-20 19:03:10 +0200

    Test: special pricing strategy should apply special price for each bundle and unit price for the remaining skus

--- checkout.tests/SpecialPricingStrategyTests.cs
+++ checkout.tests/SpecialPricingStrategyTests.cs
@@ -45,9 +45,26 @@ public class SpecialPricingStrategyTests
         Randomizer randomizer = new Randomizer(42);
         int itemCount = bundleSize - randomizer.Next(1, bundleSize);
         
-        int actualTotalPrice =
-            sut.CalculateTotalPrice(itemCount);
+        int actualTotalPrice = sut.CalculateTotalPrice(itemCount);
         
         Assert.That(actualTotalPrice, Is.EqualTo(itemCount * unitPrice));
+    }
+    
+    [Test]
+    public void Given_A_List_Of_SKUs_Should_Apply_Special_Pricing_For_Each_Bundle_Of_SKU_And_Unit_Price_For_Remaining_SKUs(
+        [Range(2, 10, 2)] int bundleSize,
+        [Range(1, 10, 2)] int unitPrice,
+        [Range(20, 100, 20)] int specialPrice)
+    {
+        IPricingStrategy sut = new SpecialPricing(bundleSize, specialPrice, unitPrice);
+        
+        var randomizer = new Randomizer(42);
+        int itemCount = bundleSize + randomizer.Next(10);
+        
+        int actualTotalPrice = sut.CalculateTotalPrice(itemCount);
+        
+        int expectedTotalPrice = (itemCount / bundleSize) * specialPrice + itemCount % bundleSize * unitPrice;
+        
+        Assert.That(actualTotalPrice, Is.EqualTo(expectedTotalPrice));
     }
 }
