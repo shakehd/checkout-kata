7be4c71224ea8d20e70e63cc0227bac2407fcb02
shakeel ahmad <shakhd@duck.com>
2025-10-21 08:07:08 +0200

    Ref: missing refactor

--- checkout/Checkout.cs
+++ checkout/Checkout.cs
@@ -8,12 +8,7 @@ public class Checkout(IPricingStrategyProvider pricingStrategyProvider) : ICheck
 {
     private readonly ICollection<string> _skuCodes = [];
 
-    public void Scan(NotEmptyAndNullString skuCode)
-    {
-        if (string.IsNullOrEmpty(skuCode))
-            throw new ArgumentException("SKU code cannot be null or empty.",  nameof(skuCode));
-        _skuCodes.Add(skuCode);
-    }
+    public void Scan(NotEmptyAndNullString skuCode) => _skuCodes.Add(skuCode);
 
     public int GetTotalPrice()
     {
